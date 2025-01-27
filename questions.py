56af4af66d1277485d1d60cde3f3c851c7d8a8e9
Jacob Isah <isahjacob0@gmail.com>
2025-01-27 11:23:25 +0000

    create question.py file for storing questions, options and the correct answer

---
+++ questions.py
@@ -0,0 +1,23 @@
+questions = [
+    
+        {
+            "question": "What is the capital of France?",
+            "options": ["A) Madrid", "B) Paris", "C) Berlin", "D) Rome"],
+            "answer": "B"
+        },
+        {
+            "question": "Which programming language is known as the language of AI?",
+            "options": ["A) Python", "B) Java", "C) C++", "D) Ruby"],
+            "answer": "A"
+        },
+        {
+            "question": "What does CPU stand for?",
+            "options": ["A) Central Processing Unit", "B) Central Performance Unit", "C) Computer Personal Unit", "D) Central Program Unit"],
+            "answer": "A"
+        },
+        {
+            "question": "Which planet is known as the Red Planet?",
+            "options": ["A) Earth", "B) Mars", "C) Venus", "D) Jupiter"],
+            "answer": "B"
+        },
+    ]
